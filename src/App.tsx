578143128a6b10e61b979d1c8ddd78c235b2f8d0
Jaehong Lee <ja34luv@gmail.com>
2026-03-11 19:52:19 -0700

    feat: add first section

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ function App() {
   return (
     <>
       {/* Navigation */}
-      <nav className="bg-background fixed w-full z-20 top-0 border-b border-divider">
+      <nav className="bg-background fixed w-full z-20 top-0 border-b border-divider min-w-xs">
         <div className="flex items-center justify-between p-4 md:py-0 md:px-8">
 
-          <a href="/" className="flex items-center gap-3">
-            <img src="/logos/d.svg" className="h-8" alt="Duri Logo" />
-            <img src="/logos/duri.svg" className="h-4" alt="Duri Logo" />
+          <a href="/">
+            <img src="/logos/duri.svg" className="h-5" alt="Duri Logo" />
           </a >
 
           <div className="inline-flex items-center gap-3 md:py-4">
@@ -23,9 +22,18 @@ function App() {
           </div>
 
         </div>
-      </nav >
+      </nav>
 
-      <div className="w-full h-[500vh]"></div>
+      <div className="w-full mt-[67px] md:mt-[77px] min-w-xs relative">
+        <div className="absolute inset-0 bg-size-[105px_105px] bg-[linear-gradient(90deg,rgba(0,0,0,.55)_1px,transparent_0),linear-gradient(180deg,rgba(0,0,0,.55)_1px,transparent_0)] opacity-10 z-5" />
+        <div className="absolute inset-0 bg-[linear-gradient(0deg,#fff,transparent)] z-10" />
+        <div className="w-full p-5 flex flex-col justify-center items-center text-center relative z-15">
+          <div className="mb-11.25">Introducing the Fetch API: One call to pull data from any URL</div>
+          <img src="/logos/d.svg" className="h-8" alt="Duri Logo" />
+          <h1>We help AI use the web.</h1>
+          <p className="mt-3.75">Autonomously read, write, and perform tasks on the web with a headless browser.</p>
+        </div>
+      </div>
     </>
   )
 }
